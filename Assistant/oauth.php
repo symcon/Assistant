046d063ecddaf199e5ad1e11ceaa2fcd9df93d2b
Michael Maroszek <maroszek@gmx.net>
2017-12-30 00:41:32 +0100

    poorly fix error messages on startup
    we should properly derive from the WebOAuthModule class

--- Assistant/oauth.php
+++ Assistant/oauth.php
@@ -7,7 +7,7 @@ trait WebOAuth
     private function RegisterOAuth($WebOAuth): void
     {
         $ids = IPS_GetInstanceListByModuleID('{F99BF07D-CECA-438B-A497-E4B55F139D37}');
-        if (count($ids) > 0) {
+        if (count($ids) > 0 && (IPS_GetInstance($ids[0])['InstanceStatus'] == 102 /* IS_ACTIVE*/)) {
             $clientIDs = json_decode(IPS_GetProperty($ids[0], 'ClientIDs'), true);
 
             //Search or Update WebHook client to our instanceID
