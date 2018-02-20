c4884ca0283750522c57c8dda108427e70167536
Niels Thole <niels.thole@gmail.com>
2018-02-20 11:14:23 +0100

    Add scenes

---
+++ Assistant/traits/sceneDeactivatable.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+class DeviceTraitSceneDeactivatable
+{
+    const propertyPrefix = 'SceneDeactivatable';
+
+    use HelperStartScript;
+
+    public static function getColumns()
+    {
+        return [
+            [
+                'label' => 'Activate Script',
+                'name'  => self::propertyPrefix . 'ActivateID',
+                'width' => '200px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectScript'
+                ]
+            ],
+            [
+                'label' => 'Deactivate Script',
+                'name'  => self::propertyPrefix . 'DeactivateID',
+                'width' => '200px',
+                'add'   => 0,
+                'edit'  => [
+                    'type' => 'SelectScript'
+                ]
+            ]
+        ];
+    }
+
+    public static function getStatus($configuration)
+    {
+        $activateStatus = self::getScriptCompatibility($configuration[self::propertyPrefix . 'ActivateID']);
+        return ($activateStatus != 'OK') ? $activateStatus : self::getScriptCompatibility($configuration[self::propertyPrefix . 'DeactivateID']);
+    }
+
+    public static function doQuery($configuration)
+    {
+        return [
+            'online' => (self::getStatus($configuration) == 'OK')
+        ];
+    }
+
+    public static function doExecute($configuration, $command, $data)
+    {
+        switch ($command) {
+            case 'action.devices.commands.ActivateScene':
+                $scriptID = $data['deactivate'] ? $configuration[self::propertyPrefix . 'ActivateID'] : $configuration[self::propertyPrefix . 'DeactivateID'];
+                if (self::startScript($scriptID, !$data['deactivate'])) {
+                    return [
+                        'ids'    => [$configuration['ID']],
+                        'status' => 'SUCCESS',
+                        'states' => [
+                            'online' => true
+                        ]
+                    ];
+                } else {
+                    return [
+                        'ids'       => [$configuration['ID']],
+                        'status'    => 'ERROR',
+                        'errorCode' => 'deviceTurnedOff'
+                    ];
+                }
+                break;
+            default:
+                throw new Exception('Command is not supported by this trait!');
+        }
+    }
+
+    public static function supportedTraits()
+    {
+        return [
+            'action.devices.traits.Scene'
+        ];
+    }
+
+    public static function supportedCommands()
+    {
+        return [
+            'action.devices.commands.ActivateScene'
+        ];
+    }
+
+    public static function getAttributes()
+    {
+        return [
+            'sceneReversible' => true
+        ];
+    }
+}
---
+++ Assistant/types/sceneDeactivatable.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+class DeviceTypeSceneDeactivatable
+{
+    private static $implementedType = 'SCENE';
+
+    private static $implementedTraits = [
+        'Scene'
+    ];
+
+    use HelperDeviceType;
+
+    public static function getPosition()
+    {
+        return 4;
+    }
+
+    public static function getCaption()
+    {
+        return 'Scenes (Deactivatable)';
+    }
+}
+
+DeviceTypeRegistry::register('SceneDeactivatable');
